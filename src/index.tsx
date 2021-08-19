aaf5eda3afa9bccc83fc023c0e94394ef6aba1f1
Yuta Ohira <alesion90@gmail.com>
2021-08-20 00:06:48 +0900

    fix: index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,7 @@ import ReactDOM from "react-dom";
 import SwaggerUI from "swagger-ui-react";
 import "swagger-ui-react/swagger-ui.css";
 
-const App: React.VFC = () => (
-  <div>
-    <SwaggerUI url="swagger.yaml" />
-  </div>
-);
+const App: React.VFC = () => <SwaggerUI url="swagger.yaml" />;
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
