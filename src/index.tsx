7d8d6a702e7ed847fa926ddbf4efbf0fd749a470
Yuta Ohira <alesion90@gmail.com>
2021-08-19 22:06:47 +0900

    yarn format

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SwaggerUI from "swagger-ui-react";
-
 import "swagger-ui-react/swagger-ui.css";
-const App = () => (
-    <div className="App">
-        <SwaggerUI url="https://api.mna.dev.spsapps.net/swagger/?format=openapi" />
-    </div>
-)
+
+const App: React.VFC = () => (
+  <div className="App">
+    <SwaggerUI url="https://api.mna.dev.spsapps.net/swagger/?format=openapi" />
+  </div>
+);
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
